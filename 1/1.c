26f95207ee26204b3ce625f15cc191cb30cb35ec
root <root@dev.localdomain>
2015-01-02 20:25:40 +0100

    Added first ctf. Super easy one.

---
+++ 1/1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef void (*func)();
+
+int main(int argc, char **argv)
+{
+	char buf[256] = {0};
+
+	printf("Enter code to execute.\n");
+
+	fgets(buf, sizeof(buf), stdin);
+	
+	printf("Read [%d] bytes.\n", (int)strlen(buf));
+
+	((func)(buf))();
+	
+	printf("Thanks.\n");	
+
+	return 0;
+}
+
